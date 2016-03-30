61bb49d01af9bf6c6f8f6b27e86084881833be78
Dan Nemrodov <dannem@gmail.com>
2016-03-30 11:15:02 -0400

    Update

--- build_face_space_proto.m
+++ build_face_space_proto.m
@@ -9,27 +9,29 @@ elseif isscalar(ndims)
 end
 
 %% convert ims from cell to double
-for i=length(ims)
-    im_mat(:,:,:,i)=ims{i};
+for i=1:length(ims)
+    im_mat(:,:,:,i)=double(ims{i});
 end
 
 %% compute porototypes
-[Y,e] = cmdscale(conf)
+[Y,e] = cmdscale(conf);
 zconf=zscore(Y);
 for i=ndims
     ind_pos=find(zconf(:,i)>0);
     ind_neg=find(zconf(:,i)<0);
     im_mat_neg=im_mat(:,:,:,ind_neg);
     im_mat_pos=im_mat(:,:,:,ind_pos);
-    prot_pos=sum(im_mat_pos.*repmat(zconf(ind_pos,i),size(im_mat,1),...
-        size(im_mat,2),3,1),2);
-    prot_neg=sum(im_mat_neg.*repmat(zconf(ind_neg,i),size(im_mat,1),...
-        size(im_mat,2),3,1),2);
+    prot_pos=sum(im_mat_pos.*repmat(reshape(zconf(ind_pos,i),1,1,1,length(ind_pos)),size(im_mat,1),...
+        size(im_mat,2),3),4);
+    prot_neg=sum(im_mat_neg.*repmat(reshape(zconf(ind_neg,i),1,1,1,length(ind_neg)),size(im_mat,1),...
+        size(im_mat,2),3),4);
+    prot_pos=prot_pos/max(prot_pos(:))*255;
+    prot_neg=prot_neg/min(prot_neg(:))*255;
     figure
-    imagesc(prot_pos)
+    imshow(uint8(prot_pos))
     title(['Dimension ' num2str(i) ', positive']);
     figure
-    imagesc(prot_neg)
+    imshow(uint8(prot_neg))
     title(['Dimension ' num2str(i) ', negative']);
 end
 
